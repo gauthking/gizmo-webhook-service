67795a626046aea998211c75088c541926a97c6c
Gautham Krishna <90638995+gauthking@users.noreply.github.com>
2023-11-29 23:09:41 +0530

    organized file structure

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import {
     addAlchemyContextToRequest,
     validateAlchemySignature,
     AlchemyWebhookEvent,
-} from "./webhooksUtil";
+} from "./helpers/webhooksUtil";
 import axios from "axios";
 import { Alchemy, Network } from "alchemy-sdk";
 import { defaultAbiCoder } from "ethers/lib/utils";
 import { ethers } from "ethers";
+import { orderFulfilledTopic, transferBatchTopic, transferSingleTopic, transferTopic } from "./helpers/utils";
 const sdk = require('api')('@opensea/v2.0#1nqh2zlnvr1o4h');
 
 async function main(): Promise<void> {
@@ -42,25 +43,25 @@ async function main(): Promise<void> {
 
         const transferTopics: any[] = data.logs.filter((dta: any) =>
             dta.topics.includes(
-                "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
+                transferTopic
             )
         );
 
         const transferSingleTopics: any[] = data.logs.filter((dta: any) =>
             dta.topics.includes(
-                "0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62"
+                transferSingleTopic
             )
         );
 
         const orderFulfilledTopicsSeaport: any[] = data.logs.filter((dta: any) =>
             dta.topics.includes(
-                "0x9d9af8e38d66c62e2c12f0225249fd9d721c54b83f48d9352c97c6cacdcb6f31"
+                orderFulfilledTopic
             )
         );
 
         const transferBatchTopics: any[] = data.logs.filter((dta: any) =>
             dta.topics.includes(
-                "0x4a39dc06d4c0dbc64b70af90fd698a233a518aa5d07e595d983b8c0526c8f7fb"
+                transferBatchTopic
             )
         );
 
@@ -93,16 +94,6 @@ async function main(): Promise<void> {
                         sum += parseInt(r.data);
                         tkaddress = r.address;
                     }
-                    // else {
-                    //     //nft transfer
-                    //     const address = r.address;
-                    //     NFTDATA.push({
-                    //         // token_id: token_id,
-                    //         address: address,
-                    //     });
-                    //     tkaddress = "ETH"
-                    //     sum = webhookEvent.event.data.block.transactions[0].value
-                    // }
                 }
             });
         }
@@ -130,20 +121,18 @@ async function main(): Promise<void> {
                                     nftAddress: token_address,
                                 });
                             }
-
                         }
+                        // for getting erc20s from seaport
                         if (offer.itemType === 1) {
                             ERC.push({
                                 tokenAddress: offer.token,
                                 value: offer.endAmount
                             });
                         }
-
                     })
                 } catch (error) {
                     console.log("an err occured at fetching nftorder");
                 }
-
             })
         }
 
--- src/utils.ts
+++
@@ -1,1 +0,0 @@
-export const transferSignatures: Array<string> = ["0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"]
